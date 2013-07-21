03e7b66e8a76e32a03844a74ba01bcbef88370ec
JC Brand <jc@opkode.com>
2013-07-21 21:23:50 +0200

    Add more tests

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -6,11 +6,20 @@
         }
     );
 } (this, function (klokwerk, mock_connection) {
+
+    createTask = function (desc, tags) {
+        var $form = klokwerk.trackerview.$el.find('form.tracker-form');
+        $form.find('input#task-name').val(desc);
+        $form.find('input#labels').val(tags.join(','));
+        $form.submit();
+    };
+
     return describe("The Tracker", $.proxy(function () {
-        
+
         beforeEach (function () {
             window.localStorage.clear();
-            $('#current-tasks-section').hide().find('ul.tasklist:first').empty();
+            $('#current-tasks-section').hide().find('ul.tasklist').empty();
+            $('#finished-tasks-section').hide().find('ul.tasklist').empty();
         });
 
         describe('The current tasks section', $.proxy(function () {
@@ -19,12 +28,34 @@
             }, klokwerk));
         
             it('allows the creation of a new task', $.proxy(function () {
-                var $form = klokwerk.trackerview.$el.find('form.tracker-form');
-                $form.find('input#task-name').val('Writing a blog post');
-                $form.submit();
-                expect($('#current-tasks-section').is(':visible')).toEqual(true);
-                expect($('#current-tasks-section').hide().find('ul.tasklist').children().length, 1);
+                createTask('Writing a book', ['writing', 'book']);
+                var $section = $('#current-tasks-section');
+                expect($section.is(':visible')).toEqual(true);
+                expect($section.find('ul.tasklist').children('li').length).toEqual(1);
+                var $task = $section.find('li');
+                expect($task.find('span.label').length).toEqual(2);
+                expect($task.find('span.label:first').text()).toEqual('writing');
+                expect($task.find('span.label:last').text()).toEqual('book');
             }, klokwerk));
+        }));
+
+        describe('The current task', $.proxy(function () {
+            it('is automatically stopped when a new task is created', $.proxy(function () {
+                var $section = $('#current-tasks-section');
+                expect($section.is(':visible')).toEqual(false);
+                runs(function () {
+                    createTask('Writing a book', ['writing', 'book']);
+                });
+                waits(250);
+                runs(function () {
+                    expect($section.is(':visible')).toEqual(true);
+                    createTask('Editing a play', ['editing', 'play']);
+                });
+                waits(250);
+                runs(function () {
+                    expect($section.is(':visible')).toEqual(true);
+                });
+            }));
         }));
 
         describe('The finished tasks section', $.proxy(function () {
