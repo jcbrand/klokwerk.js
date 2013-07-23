33b2f04f8502f2ac7a8894ccffbf65244bfff093
JC Brand <jc@opkode.com>
2013-07-23 12:57:26 +0200

    Add some code to create tasks on various dates

--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -7,28 +7,23 @@
     );
 } (this, function (klokwerk, mock_connection) {
 
-    createTask = function (desc, tags) {
+    createTaskFromForm = function (desc, tags) {
         var $form = klokwerk.trackerview.$el.find('form.tracker-form');
         $form.find('input#task-name').val(desc);
         $form.find('input#labels').val(tags.join(','));
         $form.submit();
     };
-
     return describe("The Tracker", $.proxy(function () {
-
-        beforeEach (function () {
+        afterEach(function () {
             window.localStorage.clear();
             $('#current-tasks-section').hide().find('ul.tasklist').empty();
             $('#finished-tasks-section').hide().find('ul.tasklist').empty();
         });
 
-        describe('The current tasks section', $.proxy(function () {
-            it("shows nothing if there aren't any tasks", $.proxy(function () {
-                expect($('#current-tasks-section').is(':visible')).toEqual(false);
-            }, klokwerk));
+        describe('The task form', $.proxy(function () {
         
             it('allows the creation of a new task', $.proxy(function () {
-                createTask('Writing a book', ['writing', 'book']);
+                createTaskFromForm('Writing a book', ['writing', 'book']);
                 var $section = $('#current-tasks-section');
                 expect($section.is(':visible')).toEqual(true);
                 expect($section.find('ul.tasklist').children('li').length).toEqual(1);
@@ -44,14 +39,14 @@
                 var $section = $('#current-tasks-section');
                 expect($section.is(':visible')).toEqual(false);
                 runs(function () {
-                    createTask('Writing a book', ['writing', 'book']);
+                    createTaskFromForm('Writing a book', ['writing', 'book']);
                 });
-                waits(250);
+                waits(500);
                 runs(function () {
                     expect($section.is(':visible')).toEqual(true);
-                    createTask('Editing a play', ['editing', 'play']);
+                    createTaskFromForm('Editing a play', ['editing', 'play']);
                 });
-                waits(250);
+                waits(500);
                 runs(function () {
                     expect($section.is(':visible')).toEqual(true);
                 });
@@ -64,6 +59,30 @@
             }, klokwerk));
         
             it('shows finished tasks if there are any', $.proxy(function () {
+                runs(function () {
+                    var d = new Date(), 
+                            end_date, end_iso, i, start_date, start_iso;
+                    for (i=1; i<5; i++) {
+                        start_date = new Date(d.setDate(d.getDate()-i));
+                        start_iso = klokwerk.toISOString(klokwerk.roundDate(start_date));
+                        end_date = new Date(start_date.getTime() + 60*1000);
+                        end_iso = klokwerk.toISOString(klokwerk.roundDate(end_date));
+
+                        klokwerk.tracker.create({
+                            'description': 'Task '+i,
+                            'start': start_iso,
+                            'start_day': start_iso.split('T')[0]+'T00:00:00Z',
+                            'start_month': start_date.getUTCFullYear()+"-"+start_date.getUTCMonth()+"-1"+'T00:00:00Z',
+                            'end': end_iso,
+                            'category': '',
+                            'labels': '' 
+                        });
+                    }
+                });
+                waits(500);
+                runs(function () {
+                    alert('hello');
+                });
             }, klokwerk));
         }));
 
