b29770cc1c916fb390dea7d02c2be7daba9ae08d
JC Brand <jc@opkode.com>
2014-04-28 21:42:50 +0200

    Make tests pass again

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-
-require.config({
+config = {
     paths: {
-        "jquery": "components/jquery/jquery",
-        "jquery.placeholder": "components/jquery.placeholder/jquery.placeholder", 
-        "modernizr": "components/modernizr/modernizr",
-        "bootstrap-tooltip": "lib/bootstrap/js/bootstrap-tooltip",
-        "bootstrap-popover": "lib/bootstrap/js/bootstrap-popover",
-        "bootstrap-dropdown": "lib/bootstrap/js/bootstrap-dropdown",
-        "bootstrap-alert": "lib/bootstrap/js/bootstrap-alert",
-        "bootstrap-datepicker": "lib/bootstrap-datepicker",
-        "select2": "components/select2/select2",
-        "jquery.validate": "components/jquery-validation/jquery.validate",
-        "locales": "locale/locales",
-        "underscore": "components/underscore//underscore",
         "backbone": "components/backbone/backbone",
         "backbone.localStorage": "components/backbone.localStorage/backbone.localStorage",
+        "bootstrap-alert": "lib/bootstrap/js/bootstrap-alert",
+        "bootstrap-datepicker": "lib/bootstrap-datepicker",
+        "bootstrap-dropdown": "lib/bootstrap/js/bootstrap-dropdown",
+        "bootstrap-popover": "lib/bootstrap/js/bootstrap-popover",
+        "bootstrap-tooltip": "lib/bootstrap/js/bootstrap-tooltip",
+        "jquery": "components/jquery/jquery",
+        "jquery.placeholder": "components/jquery.placeholder/jquery.placeholder", 
+        "jquery.validate": "components/jquery-validation/dist/jquery.validate",
+        "locales": "locale/locales",
+        "modernizr": "components/modernizr/modernizr",
+        "moment": "components/momentjs/moment",
+        "select2": "components/select2/select2",
+        "underscore": "components/underscore//underscore",
         "klokwerk-dependencies": "src/deps-full"
     },
-
     // define module dependencies for modules not using define
     shim: {
         'backbone': {
@@ -33,12 +32,13 @@ require.config({
         },
         'underscore':   { exports: '_' }
     }
-});
+};
 
-require(["klokwerk"],
-    function(klokwerk) {
+if (typeof(require) !== 'undefined') {
+    require.config(config);
+    require(["klokwerk"], function(klokwerk){
         $(document).ready(function () {
             klokwerk.initialize();
         });
-    }
-);
+    });
+}
--- tests_main.js
+++ tests_main.js
@@ -1,70 +1,39 @@
-require.config({
-    paths: {
-        "jquery": "components/jquery/jquery",
-        "jquery.placeholder": "lib/jquery.placeholder", 
-        "modernizr": "lib/modernizr",
-        "bootstrap-tooltip": "lib/bootstrap/js/bootstrap-tooltip",
-        "bootstrap-popover": "lib/bootstrap/js/bootstrap-popover",
-        "bootstrap-dropdown": "lib/bootstrap/js/bootstrap-dropdown",
-        "bootstrap-alert": "lib/bootstrap/js/bootstrap-alert",
-        "bootstrap-datepicker": "lib/bootstrap-datepicker",
-        "select2": "lib/select2-3.3.1/select2",
-        "jquery.validate": "lib/jquery-validation/jquery.validate",
-        "locales": "locale/locales",
-        "underscore": "lib/underscore",
-        "backbone": "lib/backbone",
-        "backbone.localStorage": "lib/backbone.localStorage",
-        "klokwerk-dependencies": "src/deps-full",
-        // Extra test dependencies
-        "mock": "tests/mock",
-        "utils": "tests/utils",
-        "jasmine": "components/jasmine/lib/jasmine-core/jasmine",
-        "jasmine-html": "components/jasmine/lib/jasmine-core/jasmine-html",
-        "jasmine-console-reporter": "node_modules/jasmine-reporters/src/jasmine.console_reporter",
-        "jasmine-junit-reporter": "node_modules/jasmine-reporters/src/jasmine.junit_reporter"
-    },
-
-    // define module dependencies for modules not using define
-    shim: {
-        'backbone': {
-            //These script dependencies should be loaded before loading
-            //backbone.js
-            deps: [
-                'underscore',
-                'jquery'
-                ],
-            //Once loaded, use the global 'Backbone' as the
-            //module value.
-            exports: 'Backbone'
-        },
-        'underscore':   { exports: '_' },
-        // Extra test dependencies
-        'jasmine-html': {
-            deps: ['jasmine'],
-            exports: 'jasmine'
-        },
-        'jasmine-console-reporter': {
-            deps: ['jasmine-html'],
-            exports: 'jasmine'
-        },
-        'jasmine-junit-reporter': {
-            deps: ['jasmine-html'],
-            exports: 'jasmine'
-        }
-    }
-});
+// Extra test dependencies
+config.paths.mock = "tests/mock";
+config.paths.utils = "tests/utils";
+config.paths.jasmine = "components/jasmine/lib/jasmine-core/jasmine";
+config.paths["jasmine-html"] = "components/jasmine/lib/jasmine-core/jasmine-html";
+config.paths["console-runner"] = "node_modules/phantom-jasmine/lib/console-runner";
+config.shim['jasmine-html'] = {
+    deps: ['jasmine'],
+    exports: 'jasmine'
+};
+require.config(config);
 
 require([
-    "spec/tracker"
-    ], function() {
-        // Jasmine stuff
-        var jasmineEnv = jasmine.getEnv();
-        jasmineEnv.updateInterval = 250;
-        var htmlReporter = new jasmine.HtmlReporter();
-        jasmineEnv.addReporter(htmlReporter);
-        jasmineEnv.specFilter = function(spec) {
-            return htmlReporter.specFilter(spec);
-        };
-        jasmineEnv.execute();
+    "jquery",
+    "klokwerk",
+    "mock",
+    "jasmine-html"
+    ], function($, klokwerk, mock, jasmine) {
+        require(["spec/tracker"], function() {
+            // Jasmine stuff
+            var jasmineEnv = jasmine.getEnv();
+            var reporter;
+            if (/PhantomJS/.test(navigator.userAgent)) {
+                reporter = new jasmine.ConsoleReporter();
+                window.console_reporter = reporter;
+                jasmineEnv.addReporter(reporter);
+                jasmineEnv.updateInterval = 0;
+            } else {
+                reporter = new jasmine.HtmlReporter();
+                jasmineEnv.addReporter(reporter);
+                jasmineEnv.specFilter = function(spec) {
+                    return reporter.specFilter(spec);
+                };
+                jasmineEnv.updateInterval = 0;
+            }
+            jasmineEnv.execute();
+        });
     }
 );
